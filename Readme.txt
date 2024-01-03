1d94ae4e3a6f110caae5ad159619b2d6b27cb96b
furkanermis <furkan.ermis@icloud.com>
2024-01-03 16:17:49 +0300

    CREATE

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+Adem Karagülle
+Furkan Ermiş
+İsmail Arif Güleç
+İrem Nisa Gündoğdu
+Dilara Özdemir
---
+++ TravelGuide/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelGuide.Startup))]
+namespace TravelGuide
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
